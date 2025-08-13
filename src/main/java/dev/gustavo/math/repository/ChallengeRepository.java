55da3428c1f53002090d37e980148df46df2bc33
gustavo <geraldellig@gmail.com>
2025-08-13 19:14:48 -0300

    feat: create custom query

--- src/main/java/dev/gustavo/math/repository/ChallengeRepository.java
+++ src/main/java/dev/gustavo/math/repository/ChallengeRepository.java
@@ -2,6 +2,13 @@ package dev.gustavo.math.repository;
 
 import dev.gustavo.math.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+
+    @Query("SELECT c FROM Challenge c LEFT JOIN FETCH c.testCases WHERE c.id = :id")
+    Optional<Challenge> findByIdWithTestCases(Long id);
+
 }
