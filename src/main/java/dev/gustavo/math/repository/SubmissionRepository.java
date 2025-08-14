af47c1a6f1a730d180e359947e6d6eca5aeb6aa7
gustavo <geraldellig@gmail.com>
2025-08-14 13:25:39 -0300

    feat: submission join queries

--- src/main/java/dev/gustavo/math/repository/SubmissionRepository.java
+++ src/main/java/dev/gustavo/math/repository/SubmissionRepository.java
@@ -1,7 +1,19 @@
 package dev.gustavo.math.repository;
 
 import dev.gustavo.math.entity.Submission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.UUID;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+
+    @Query("SELECT s FROM Submission s JOIN FETCH s.user WHERE s.challenge.id = :id")
+    Page<Submission> findByChallengeIdWithUser(Long id, Pageable pageable);
+
+    @Query("SELECT s FROM Submission s JOIN FETCH s.challenge WHERE s.user.id = :id")
+    Page<Submission> findByUserIdWithChallenge(UUID id, Pageable pageable);
+
 }
