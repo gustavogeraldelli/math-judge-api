1e8fa5907df1282b7713b792325202df7ea0ba88
gustavo <geraldellig@gmail.com>
2025-08-14 16:43:42 -0300

    feat: handling exception from validation

--- src/main/java/dev/gustavo/math/infra/config/GlobalExceptionHandler.java
+++ src/main/java/dev/gustavo/math/infra/config/GlobalExceptionHandler.java
@@ -5,8 +5,14 @@ import dev.gustavo.math.exception.UsernameIsAlreadyInUseException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,6 +30,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidForeignKeyException.class)
     public ResponseEntity<ErrorResponseDTO> handleInvalidForeignKeyException(InvalidForeignKeyException e) {
         return new ResponseEntity<>(new ErrorResponseDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleArgumentNotValid(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
