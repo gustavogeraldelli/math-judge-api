c258d81b371868fcdc678ac9e4289624c01d8208
gustavo <geraldellig@gmail.com>
2025-08-14 14:34:14 -0300

    feat: add endpoint to list user submissions

--- src/main/java/dev/gustavo/math/controller/UserController.java
+++ src/main/java/dev/gustavo/math/controller/UserController.java
@@ -3,6 +3,8 @@ package dev.gustavo.math.controller;
 import dev.gustavo.math.controller.dto.PageableResponseDTO;
 import dev.gustavo.math.controller.dto.user.UserRequestDTO;
 import dev.gustavo.math.controller.dto.user.UserResponseDTO;
+import dev.gustavo.math.controller.dto.user.UserSubmissionsResponseDTO;
+import dev.gustavo.math.mapper.SubmissionMapper;
 import dev.gustavo.math.mapper.UserMapper;
 import dev.gustavo.math.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -51,6 +53,16 @@ public class UserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id) {
         userService.delete(id);
+    }
+
+    @GetMapping("/{id}/submissions")
+    @ResponseStatus(HttpStatus.OK)
+    public PageableResponseDTO<UserSubmissionsResponseDTO> listUserSubmissions(@PathVariable UUID id,
+                                                                               @RequestParam(defaultValue = "0") Integer page,
+                                                                               @RequestParam(defaultValue = "10") Integer size) {
+        var userSubmissions = userService.listSubmissions(id, PageRequest.of(page, size))
+                .map(SubmissionMapper.INSTANCE::toUserSubmissionsResponseDTO);
+        return new PageableResponseDTO<>(userSubmissions);
     }
 
 }
