faf724251498262415277524d5adc674f09f70a0
gustavo <geraldellig@gmail.com>
2025-08-15 15:39:37 -0300

    feat: login dtos

---
+++ src/main/java/dev/gustavo/math/controller/dto/user/LoginRequestDTO.java
@@ -0,0 +1,12 @@
+package dev.gustavo.math.controller.dto.user;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequestDTO(
+        @NotBlank(message = "Username is required")
+        String username,
+
+        @NotBlank(message = "Password is required")
+        String password
+) {
+}
---
+++ src/main/java/dev/gustavo/math/controller/dto/user/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package dev.gustavo.math.controller.dto.user;
+
+public record LoginResponseDTO(String token) {
+}
