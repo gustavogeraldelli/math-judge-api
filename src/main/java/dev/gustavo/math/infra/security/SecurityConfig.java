9506854f1014a58c318c944594aa441697797e6f
gustavo <geraldellig@gmail.com>
2026-07-16 12:03:25 -0300

    fix: use class-based proxies for method security

--- src/main/java/dev/gustavo/math/infra/security/SecurityConfig.java
+++ src/main/java/dev/gustavo/math/infra/security/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableMethodSecurity
+@EnableMethodSecurity(proxyTargetClass = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
